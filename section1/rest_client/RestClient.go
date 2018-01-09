bd428652a5cc1e91cedd329234815ca286a5e270
KedroBoss <dimmak90@gmail.com>
2018-01-09 12:55:58 +0300

    simple api fetcher

---
+++ section1/rest_client/RestClient.go
@@ -0,0 +1,26 @@
+// Simple Rest Api that fetches json page from the url
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
+const apiURL = "http://quotes.rest/qod.json"
+
+func main() {
+	res, err := http.Get(apiURL)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer res.Body.Close()
+
+	content, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println(string(content))
+}
